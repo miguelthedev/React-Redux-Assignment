782d646e623ebb438eca8857555cb044a8bc3ece
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-22 10:30:31 -0700

    Application now uses Redux to manage state.

--- src/store/reducers/persons.js
+++ src/store/reducers/persons.js
@@ -7,13 +7,23 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(),
+        name: 'Miguel',
+        age: Math.floor( Math.random() * 40 )
+      }
+
       return {
-        ...state
+        ...state,
+        persons: state.persons.concat(newPerson)
       }
 
     case actionTypes.DELETE_PERSON:
+      const updatedArray = state.persons.filter(person => person.id !== action.id);
+
       return {
-        ...state
+        ...state,
+        persons: updatedArray
       }
 
     default:
